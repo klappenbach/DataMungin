73ba22195883e2b540dde6b0ccb0bb92b3f81694
Gabriel Klappenbach <gabriel.klappenbach@gmail.com>
2012-02-20 14:59:51 +0100

    First commit

---
+++ src/main/scala/DataMungin/App.scala
@@ -0,0 +1,10 @@
+package DataMungin
+
+import io.Source
+
+object App extends App {
+  val filePath = args(0)
+  val statsLines = for (line <- Source.fromFile(filePath).getLines() if line.trim.nonEmpty && line.trim.head.isDigit ) yield line.trim()
+  val spreadPerDay = for(line <- statsLines) yield (line.split("\\D+")(0).toInt, line.split("\\D+")(1).toInt - line.split("\\D+")(2).toInt)
+  println(spreadPerDay.toList.sortWith( (x,y)  => x._2 < y._2).head._2 )
+}
