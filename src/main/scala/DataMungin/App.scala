72f51071b48a13103a8a26f725536b4943563e3a
Gabriel Klappenbach <gabriel.klappenbach@gmail.com>
2012-02-20 19:14:08 +0100

    replaced loops with map functions. Printing day with lowest spread value instead of the lowest spread value.

--- src/main/scala/DataMungin/App.scala
+++ src/main/scala/DataMungin/App.scala
@@ -6,10 +6,9 @@ object App extends App {
   val filePath = args.headOption.getOrElse("weather.dat")
   val lines = Source.fromFile(filePath).getLines()
   val statsLines = lines.filter(s => s.trim.nonEmpty && s.trim.head.isDigit).map(_.trim)
-  val spreadPerDay = statsLines.map(line => {
-    val split = line.split("\\D+")
-    val temperatureSpreadOfDay = split(1).toInt - split(2).toInt
-    (split(0).toInt, temperatureSpreadOfDay)
-  })
-  println(spreadPerDay.toList.sortWith( (x,y)  => x._2 < y._2).head._2 )
+  val intColumns = statsLines map (_  split ("\\D+") map (_.toInt))
+  val temperatureSpreadPerDay = intColumns map { cols => (cols(0), cols(1) - cols(2)) }
+
+  val dayWithLowestTemperatureSpread = temperatureSpreadPerDay.toList.sortWith((x, y) => x._2 < y._2).head._1
+  println(dayWithLowestTemperatureSpread )
 }
