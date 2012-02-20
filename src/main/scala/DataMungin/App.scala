517e923e61a5b015379acd9cd4cdf766f57b08c7
Gabriel Klappenbach <gabriel.klappenbach@gmail.com>
2012-02-20 21:00:38 +0100

    part 2, football table. Didn't refactor out common parts (part 3), too boring...

--- src/main/scala/DataMungin/App.scala
+++ src/main/scala/DataMungin/App.scala
@@ -2,13 +2,31 @@ package DataMungin
 
 import io.Source
 
-object App extends App {
-  val filePath = args.headOption.getOrElse("weather.dat")
-  val lines = Source.fromFile(filePath).getLines()
-  val statsLines = lines.filter(s => s.trim.nonEmpty && s.trim.head.isDigit).map(_.trim)
-  val intColumns = statsLines map (_  split ("\\D+") map (_.toInt))
-  val temperatureSpreadPerDay = intColumns map { cols => (cols(0), cols(1) - cols(2)) }
+object Weather extends App {
+    val separator = "\\D+"
 
-  val dayWithLowestTemperatureSpread = temperatureSpreadPerDay.toList.sortWith((x, y) => x._2 < y._2).head._1
-  println(dayWithLowestTemperatureSpread )
+  val filePath = args.headOption.getOrElse("weather.dat")
+  val lines = Source.fromFile(filePath).getLines().map(_.trim)
+  val statsLines = lines.filter(s => s.nonEmpty && s.head.isDigit)
+  val columns = statsLines map (_ split separator)
+
+  val temperatureSpreadPerDay = columns map { cols => (cols(0), cols(1).toInt - cols(2).toInt) }
+
+  val dayWithLowestTemperatureSpread = temperatureSpreadPerDay.toList.sortWith((x, y) => x._2 < y._2).head
+  println("Day with lowest temperature spread: day " + dayWithLowestTemperatureSpread._1 + " (spread is " + dayWithLowestTemperatureSpread._2 + ")" )
+}
+
+object Football extends App {
+    val separator = "\\s+"
+  val filePath = args.headOption.getOrElse("football.dat")
+  val lines = Source.fromFile(filePath).getLines().map(_.trim)
+  val statsLines = lines.filter(s => s.nonEmpty && s.head.isDigit)
+  val columns = statsLines map (_ split separator)
+
+
+  val scoreDiffPerTeam = columns map { cols => (cols(1), math.abs(cols(6).toInt - cols(8).toInt)) }
+
+  val teamWithLowestDiff = scoreDiffPerTeam.toList.sortWith((x, y) => x._2 < y._2).head
+  println("Team with lowest diff for/against: " + teamWithLowestDiff._1 + " (diff is " + teamWithLowestDiff._2 + ")" )
+  
 }
