4fd8a30d1e0aa38f52fcbdd70bff3633f63d7cc3
Gabriel Klappenbach <gabriel.klappenbach@gmail.com>
2012-02-20 15:14:29 +0100

    getOrElse on the file name

--- src/main/scala/DataMungin/App.scala
+++ src/main/scala/DataMungin/App.scala
@@ -3,7 +3,7 @@ package DataMungin
 import io.Source
 
 object App extends App {
-  val filePath = args.head
+  val filePath = args.headOption.getOrElse("weather.dat")
   val statsLines = for (line <- Source.fromFile(filePath).getLines() if line.trim.nonEmpty && line.trim.head.isDigit ) yield line.trim()
   val spreadPerDay = for(line <- statsLines) yield (line.split("\\D+")(0).toInt, line.split("\\D+")(1).toInt - line.split("\\D+")(2).toInt)
   println(spreadPerDay.toList.sortWith( (x,y)  => x._2 < y._2).head._2 )
